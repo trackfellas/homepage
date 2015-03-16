9a599df9f31d99f194b04d7a473a35e134400059
Niklas Hofmann <niklas.hofmann@me.com>
2015-03-16 11:07:30 +0100

    switched path of feed to blog.trackfellas.com

--- themes/trackfellas/source/js/app-min.js
+++ themes/trackfellas/source/js/app-min.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){$("form").formchimp({buttonText:"Thanks!",errorMessage:"Please enter a valid email!",responseClass:"mc-response text-center",successMessage:"Almost finished... We need to confirm your email address. To complete the subscription process, please click the link in the email we just sent you."});});$(document).foundation();smoothScroll.init();$.get("http://feedler.niklas-hofmann.de/atom.xml", function(data) {
+$(document).ready(function(){$("form").formchimp({buttonText:"Thanks!",errorMessage:"Please enter a valid email!",responseClass:"mc-response text-center",successMessage:"Almost finished... We need to confirm your email address. To complete the subscription process, please click the link in the email we just sent you."});});$(document).foundation();smoothScroll.init();$.get("http://blog.trackfellas.com/atom.xml", function(data) {
     var $data = $(data);
     $(data).find('entry').each(function() 
   {
