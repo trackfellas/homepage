e02e076a287b9bc005faf3cf2a37954f280f8945
Niklas Hofmann <niklas.hofmann@me.com>
2015-06-24 11:21:12 +0200

    config files and added featherlight

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ var gulp = require('gulp'),
     minifycss = require('gulp-minify-css'),
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat');
-
 gulp.task('sass', function() {
     var sassSettings = {
         outputStyle: 'compressed',
@@ -18,24 +17,22 @@ gulp.task('sass', function() {
         .pipe(minifycss())
         .pipe(gulp.dest('themes/trackfellas/source/css'));
 });
-
 gulp.task('scripts', function() {
     gulp.src([
       './themes/trackfellas/bower_components/jquery/dist/jquery.js',
       './themes/trackfellas/bower_components/foundation/js/foundation.js',
       './themes/trackfellas/bower_components/slick.js/slick/slick.js',
       './themes/trackfellas/js/jquery.formchimp-min.js',
+      './themes/trackfellas/bower_components/featherlight/src/featherlight.js',
       './themes/trackfellas/js/smooth-scroll.min.js',
       './themes/trackfellas/js/app.js'])
     .pipe(concat('script.js'))
     .pipe(uglify())
     .pipe(gulp.dest('./themes/trackfellas/source/js/'));
 });
-/* Watch Files For Changes */
 gulp.task('watch', function() {
     gulp.watch('themes/trackfellas/scss/*.scss', ['sass']);
     gulp.watch('themes/trackfellas/js/*.js', ['scripts']);
 });
-
 gulp.task('default', ['sass', 'scripts']);
 gulp.task('develop', ['sass', 'scripts','watch']);
