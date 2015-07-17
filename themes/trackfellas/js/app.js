b43a47f5f706e9cb31b5bd380f3cc17780bd0728
Niklas Hofmann <niklas.hofmann@me.com>
2015-07-17 17:48:52 +0200

    making social media icons ready for responsive.

--- themes/trackfellas/js/app.js
+++ themes/trackfellas/js/app.js
@@ -1,3 +1,19 @@
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '741343615976832',
+        xfbml      : true,
+        version    : 'v2.4'
+    });
+};
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
 $(document).ready(function() {
     if ($(".galerie").length > 0) {
         $(".galerie").slick({
